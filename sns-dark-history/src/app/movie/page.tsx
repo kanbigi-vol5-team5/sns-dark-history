1ea71d46993f66461b21d113155225b400f4651d
Mai0076 <makuma222@gmail.com>
2025-03-18 03:16:58 +0900

    decent-person

--- sns-dark-history/src/app/movie/page.tsx
+++ sns-dark-history/src/app/movie/page.tsx
@@ -3,20 +3,7 @@ import Link from "next/link";
 export const metadata = {
   title: "SNS | movie",
 };
-const texts = [
-  "黒歴史ツイートその1",
-  "恥ずかしいポスト2過去のヤバい発言3過去のヤバい発言3",
-  "過去のヤバい発言3",
-  "過去のヤバい発言3",
-  "過去のヤバい発言3",
-  "過去のヤバい発言3",
-  "過去のヤバい発言3",
-  "過去のヤバい発言3",
-  ,
-  "過去のヤバい発言3",
-  "過去のヤバい発言3",
-  "過去のヤバい発言3",
-];
+const texts = [];
 
 export default function Page() {
   return (
@@ -24,14 +11,27 @@ export default function Page() {
       <div className="m-8 flex items-center justify-center flex-col">
         <h1 className="text-3xl font-bold">黒歴史を振り返る</h1>
         <div className="p-4 max-w-2xl mx-auto">
-          <h1 className="text-xl font-bold">あなたの黒歴史は？</h1>
-          <ul className="list-disc list-inside bg-white p-4 rounded-md">
-            {texts.map((text, index) => (
-              <li key={index} className="text-gray-800 mb-2">
-                {text}
-              </li>
-            ))}
-          </ul>
+          <h1 className="text-xl font-bold text-center">あなたの黒歴史は？</h1>
+          {texts.length > 0 ? (
+            <ul className="list-disc list-inside bg-white p-4 rounded-md">
+              {texts.map((text, index) => (
+                <li key={index} className="text-gray-800 mb-2">
+                  {text}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div className="text-center">
+              <p className="text-xl text-blue-700 mt-3">
+                あなたはまともな人間です
+              </p>
+              <img
+                src="/img/decent-person.jpg"
+                alt="まともな人間"
+                className="mt-4 mx-auto"
+              />
+            </div>
+          )}
         </div>
         <div className="items-center justify-center flex flex-col">
           <button className="mt-7 px-9 py-3 w-48 bg-black text-white rounded-full shadow-md hover:bg-red-600">
