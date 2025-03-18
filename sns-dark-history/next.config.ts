e6d434c35b9f1d5aea7e25871f71ac753a3aef0a
taiseiue <taisei@wsoft.ws>
2025-03-19 08:52:56 +0900

    エンドポイントを呼べないのを修正

--- sns-dark-history/next.config.ts
+++ sns-dark-history/next.config.ts
@@ -9,16 +9,8 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/dark_posts/:user_id',
-        destination: 'http://backend:5001/dark_posts/:user_id',
-      },
-      {
-        source: '/api/posts/:user_id',
-        destination: 'http://backend:5001/posts/:user_id',
-      },
-      {
-        source: '/api/icon/:user_id',
-        destination: 'http://backend:5001/icon/:user_id',
+        source: '/api/:subpath',
+        destination: 'http://backend:5001/:subpath',
       },
     ]
   },
