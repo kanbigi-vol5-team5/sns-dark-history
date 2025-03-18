09f6dcca20d137afdc8c1876a903fbda53758bec
Mai0076 <makuma222@gmail.com>
2025-03-18 10:59:12 +0900

    list-fix

--- sns-dark-history/src/app/[account_id]/page.tsx
+++ sns-dark-history/src/app/[account_id]/page.tsx
@@ -1,25 +1,30 @@
 "use client";
-import useSWR from 'swr';
+import useSWR from "swr";
 import Link from "next/link";
-import { useParams } from 'next/navigation';
+import { useParams } from "next/navigation";
 
 async function fetcher(key: string) {
   return fetch(key).then((res) => res.json() as Promise<Array<string> | null>);
 }
 
-export default  function Page() {
+export default function Page() {
   const { account_id } = useParams();
-  const { data, error, isLoading } = useSWR(`/api/dark_posts/${account_id}`, fetcher);
+  const { data, error, isLoading } = useSWR(
+    `/api/dark_posts/${account_id}`,
+    fetcher
+  );
   if (error) return <div>エラーです</div>;
-  if(isLoading){
-    return (<>
-      <div className="items-center justify-center flex flex-col min-h-screen">
-        <video width="300" height="300" loop autoPlay muted>
-          <source src="/img/loading.mp4" type="video/mp4" />
-          動画が表示されていません
-        </video>
-      </div>
-    </>);
+  if (isLoading) {
+    return (
+      <>
+        <div className="items-center justify-center flex flex-col min-h-screen">
+          <video width="300" height="300" loop autoPlay muted>
+            <source src="/img/loading.mp4" type="video/mp4" />
+            動画が表示されていません
+          </video>
+        </div>
+      </>
+    );
   }
   return (
     <>
@@ -28,9 +33,9 @@ export default  function Page() {
         <div className="p-4 max-w-2xl mx-auto">
           <h1 className="text-xl font-bold text-center">あなたの黒歴史は？</h1>
           {data!.length > 0 ? (
-            <ul className="list-disc list-inside bg-white p-4 rounded-md">
+            <ul className="list-disc list-outside bg-white p-4 rounded-md">
               {data!.map((text, index) => (
-                <li key={index} className="text-gray-800 mb-2">
+                <li key={index} className="text-gray-800 mb-2 break-words ">
                   {text}
                 </li>
               ))}
